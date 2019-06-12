dd47858f201ce322c7103fb960c610c3dfa74769
Matteo Bilotta <mbilotta@linkgroup.it>
2019-06-12 10:05:11 +0200

    [ADD] Added WIP files.

---
+++ bin/powergit/branch.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+#
+
+function powerGitBranch()
+{
+    case "${1}" in
+    new)
+        echo "Not implemented yet!"
+        echo "Please, come back later! :)"
+        ;;
+    *)
+        echo "Command '${1}' not found! :("
+        ;;
+    esac
+}
